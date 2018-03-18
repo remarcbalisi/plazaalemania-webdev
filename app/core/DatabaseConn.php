22a0b7210362a0a6a73f75af185e6f4d18a63407
remarcbalisi <remarcbalisi@gmail.com>
2018-03-19 04:52:32 +0800

    openshift config

--- app/core/DatabaseConn.php
+++ app/core/DatabaseConn.php
@@ -1,15 +1,16 @@
 <?php
 class DatabaseConn{
 
-    protected $servername = "localhost";
-    protected $username = "root";
-    protected $password = "";
-    protected $dbname = "plazaalemaniawebdev";
+    protected $servername = getenv("MYSQL_SERVICE_SERVICE_HOST");
+    protected $port = getenv("MYSQL_SERVICE_SERVICE_PORT");
+    protected $username = getenv("MYSQL_USER");
+    protected $password = getenv("MYSQL_ROOT_PASSWORD");
+    protected $dbname = getenv("MYSQL_DATABASE");
     public $connection;
 
     public function __construct(){
         // Create connection
-        $this->connection = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+        $this->connection = new mysqli($this->servername.":".$port, $this->username, $this->password, $this->dbname);
 
         // Check connection
         if ($this->connection->connect_error) {

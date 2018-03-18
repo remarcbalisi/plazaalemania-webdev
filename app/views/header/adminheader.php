74d7e59e73b0d9e1770c1d221dbb73a2abe5d041
remarcbalisi <remarcbalisi@gmail.com>
2018-03-18 19:01:07 +0800

    initial layout for admin home

--- app/views/header/adminheader.php
+++ app/views/header/adminheader.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- STYLES -->
-        <link rel="stylesheet" href="<?php echo Globals::baseUrl(); ?>/public/admin/css/style2.css">
-        <link rel="stylesheet" href="<?php echo Globals::baseUrl(); ?>/public/admin/css/jquery-ui.css">
-        <!-- STYLES END -->
+    <!-- STYLES -->
+    <link rel="stylesheet" href="<?php echo Globals::baseUrl(); ?>/public/admin/css/style3.css">
+    <link rel="stylesheet" href="<?php echo Globals::baseUrl(); ?>/public/admin/css/jquery-ui.css">
+    <!-- STYLES END -->
 
-        <!-- JQUERY -->
-        <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-        <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <!-- JQUERY -->
+    <script type="text/javascript" src="<?php echo Globals::baseUrl(); ?>/public/admin/js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo Globals::baseUrl(); ?>/public/admin/js/jquery-ui.js"></script>
 
-        <title><?php echo GLobals::$title_page; ?></title>
-    </head>
-    <body>
+</head>
+<body>
+
+<div class="sidenav">
+  <a href="#about">Dashboard</a>
+  <a href="#services">Rooms</a>
+  <a href="#services">Services</a>
+  <a href="#clients">Clients</a>
+  <a href="#contact">Contact</a>
+</div>
+
+</body>
+</html>
